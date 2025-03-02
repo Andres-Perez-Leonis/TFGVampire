69b1507852bcd1e97af39199e357428d01d05a9a
AndresPerez <apleonis@gmail.com>
2025-03-02 13:28:30 +0100

    Vampire can run

--- Scripts/VampireMovement.cs
+++ Scripts/VampireMovement.cs
@@ -11,6 +11,9 @@ public partial class VampireMovement : Node
 	private double _gravity = 1000;
 
 	private Vector2 _velocity;
+
+	private bool _isRunning = false;
+    private bool _wasRunningWhenJumped = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -19,8 +22,17 @@ public partial class VampireMovement : Node
 	}
 
 
-	public void Running(bool isRunning) {
-		_currentSpeed = isRunning ? _vampire.Speed * 1.5f : _vampire.Speed;
+	public void UpdateSpeed() {
+		if (_vampire.IsOnFloor())
+        {
+            _currentSpeed = _isRunning ? _vampire.Speed * 1.5f : _vampire.Speed;
+            _wasRunningWhenJumped = _isRunning; // Guarda el estado de correr
+        }
+        else
+        {
+            // Si está en el aire, mantén la velocidad según si estaba corriendo al saltar
+            _currentSpeed = _wasRunningWhenJumped ? _vampire.Speed * 1.5f : _vampire.Speed;
+        }
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,6 +54,10 @@ public partial class VampireMovement : Node
 		}
 
 		_direction = Input.GetAxis("ui_left", "ui_right");
+
+		_isRunning = Input.IsKeyPressed(Key.Shift);
+
+		UpdateSpeed();
 
 		_velocity.X = _direction * _currentSpeed;
 		_vampire.Velocity = _velocity;
