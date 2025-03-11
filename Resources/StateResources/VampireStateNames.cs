b01599c92dc5abef981806d85426f1f8f92f4ead
AndresPerez <apleonis@gmail.com>
2025-03-11 19:36:53 +0100

    Resource File with Vampire State Names

---
+++ Resources/StateResources/VampireStateNames.cs
@@ -0,0 +1,13 @@
+using System;
+using Godot;
+public partial class VampireStateNames : Resource
+{
+	
+	public const string Idle = "IdleState";
+    public const string Hidden = "HiddenState";
+    public const string Dragging = "DraggingState";
+    public const string HidingCorpse = "HidingCorpseState";
+    public const string Moving = "MovingState";
+    public const string Attack = "AttackState";
+    public const string Talking = "TalkingState";
+}
