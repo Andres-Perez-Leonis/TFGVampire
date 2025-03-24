e786a32dac490f8711804e664dae2c5224b07f84
AndresPerez <apleonis@gmail.com>
2025-03-24 21:32:58 +0100

    Resource Files with Groups Names

---
+++ Resources/Groups/NameGroups.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+/**
+	Class for keep Group names in game
+
+	- To avoid the magic string
+
+*/
+public partial class NameGroups : Resource
+{
+	
+	public const string Routes = "Routes";
+	public const string InterconnectionRoute = "InterconnectionRoute";
+	public const string NPCsRoutes = "NPCsRoutes";
+}
