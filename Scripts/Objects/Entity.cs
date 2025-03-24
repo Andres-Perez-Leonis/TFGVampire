2d181942c6f3fea40d767a146ea1406617dbb366
AndresPerez <apleonis@gmail.com>
2025-03-24 21:05:34 +0100

    Force, Speed and AnimationPlayer Attributes

--- Scripts/Objects/Entity.cs
+++ Scripts/Objects/Entity.cs
@@ -1,20 +1,47 @@
 using Godot;
-using System;
+
+/**
+	Entity class is the base of all character
+		Extends of CharacterBody2D, since they will move and interact with the world
+*/
 
 public partial class Entity : CharacterBody2D
 {
 
-	[Export]
-	private int _health  {get; set; }
-	[Export]
-	public float Speed {get; set;}
-	[Export]
-	public float Force {get; set;}
+	// Entity health value
+	[Export] private int _health;
+	// Entity speed value
+	[Export] private float _speed;
+	// Entity force value
+	[Export] private float _force;
+	// Entity animation player object (for animations)
+    [Export] private AnimationPlayer _animationPlayer;
 
+	#region Getters y Setters
+		// Get - Set entity health value
+		public int Health {
+			get => _health;
+			set => _health = Mathf.Max(0, value);
+		}
 
-	public int Health {
-		get => _health;
-		set => _health = Math.Max(0, value);
-	}
+		// Get - Set entity speed value
+		public float Speed {
+			get => _speed;
+			set => _speed = value;
+		}
+
+		// Get - Set entity force value
+		public float Force {
+			get => _force;
+			set => _force = value;
+		}
+
+		// Get entity animation player object
+		public AnimationPlayer AnimationPlayer {
+			get => _animationPlayer;
+		}
+	#endregion
+
+	
 
 }
