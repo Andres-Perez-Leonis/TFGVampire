9277f49517655e42c513f86629ef23ca1e4d546a
AndresPerez <apleonis@gmail.com>
2025-03-24 21:06:47 +0100

    Death Name State Added

--- Resources/StateResources/VampireStateNames.cs
+++ Resources/StateResources/VampireStateNames.cs
@@ -1,5 +1,12 @@
-using System;
 using Godot;
+
+/**
+	Class for keep State names for Vampire
+
+	- To avoid the magic string
+
+*/
+
 public partial class VampireStateNames : Resource
 {
 	
@@ -9,5 +16,6 @@ public partial class VampireStateNames : Resource
     public const string HidingCorpse = "HidingCorpseState";
     public const string Moving = "MovingState";
     public const string Attack = "AttackState";
+    public const string Death = "DeathState";
     public const string Talking = "TalkingState";
 }
