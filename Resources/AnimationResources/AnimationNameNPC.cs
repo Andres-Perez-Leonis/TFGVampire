d58eb894d7a205a0d3e2e5671f0bda48c2127836
AndresPerez <apleonis@gmail.com>
2025-03-24 21:32:17 +0100

    Resource Files with Vampire and NPC  State Names

---
+++ Resources/AnimationResources/AnimationNameNPC.cs
@@ -0,0 +1,17 @@
+using Godot;
+/**
+	Class for keep Animation names for NPC
+
+	- To avoid the magic string
+
+*/
+
+public partial class AnimationNameNPC : Resource
+{
+	public const string Idle = "Idle";
+	public const string Moving = "Moving";
+	public const string Talking = "Talking";
+	public const string Fleeing = "Fleeing";
+	public const string Death = "Death";
+	public const string SaveObject = "SaveObject";
+}
---
+++ Resources/AnimationResources/AnimationNameVampire.cs
@@ -0,0 +1,17 @@
+using Godot;
+/**
+	Class for keep Animation names for Vampire
+
+	- To avoid the magic string
+
+*/
+
+public partial class AnimationNameVampire : Resource
+{
+	public const string Idle = "Idle";
+	public const string Moving = "Moving";
+	public const string Talking = "Talking";
+	public const string Fleeing = "Fleeing";
+	public const string Death = "Death";
+	public const string SaveObject = "SaveObject";
+}
