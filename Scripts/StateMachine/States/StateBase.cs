aa4343a73876f3cc1287342f84788b534d898d0d
AndresPerez <apleonis@gmail.com>
2025-03-24 21:19:57 +0100

    Restructuring and Documented

---
+++ Scripts/StateMachine/States/StateBase.cs
@@ -0,0 +1,92 @@
+using Godot;
+
+public partial class StateBase : Node
+{
+    /***
+     * The node being controlled by this state (the owner of the state).
+     */
+    private Node _controlledNode;
+
+    /***
+     * The state machine that owns this state.
+     */
+    private StateMachine _stateMachine;
+
+    /***
+     * Called when the node enters the scene tree.
+     * Initializes the controlled node to be the owner of the state.
+     */
+    public override void _Ready() {
+        base._Ready();
+        _controlledNode = this.Owner;
+    }
+
+    #region Common Methods of State Machine
+
+    /***
+     * Starts the state. This method can be overridden to initialize state-specific logic.
+     */
+    public void Start() {}
+
+    /***
+     * Ends the state. This method can be overridden to handle cleanup or transition logic.
+     */
+    public void End() {}
+
+    /***
+     * Called during the process step.
+     * This method can be overridden to add logic specific to the process step.
+     * @param delta The time elapsed since the last frame.
+     */
+    public virtual void OnProcess(double delta) {}
+
+    /***
+     * Called during the physics process step.
+     * This method can be overridden to add logic specific to the physics process.
+     * @param delta The time elapsed since the last physics frame.
+     */
+    public virtual void OnPhysicsProcess(double delta) {}
+
+    /***
+     * Handles input events for this state.
+     * This method can be overridden to respond to input events.
+     * @param @event The input event to handle.
+     */
+    public virtual void OnInput(InputEvent @event) {}
+
+    /***
+     * Handles unhandled input events for this state.
+     * This method can be overridden to respond to unhandled input events.
+     * @param @event The unhandled input event.
+     */
+    public virtual void OnUnhandledInput(InputEvent @event) {}
+
+    /***
+     * Handles unhandled key input events for this state.
+     * This method can be overridden to respond to unhandled key input events.
+     * @param @event The unhandled key input event.
+     */
+    public virtual void OnUnhandledKeyInput(InputEvent @event) {}
+
+    #endregion
+
+    #region Getters and Setters
+
+    /***
+     * Gets or sets the node controlled by this state.
+     */
+    public Node ControlledNode {
+        get { return _controlledNode; }
+        set { _controlledNode = value; }
+    }
+
+    /***
+     * Gets or sets the state machine associated with this state.
+     */
+    public StateMachine StateMachine {
+        get { return _stateMachine; }
+        set { _stateMachine = value; }
+    }
+
+    #endregion
+}
