cecd400b9e696164c5997fc03a0914d1831d03a0
AndresPerez <apleonis@gmail.com>
2025-03-27 18:48:29 +0100

    Vampire Attack State Added

---
+++ Scripts/StateMachine/States/CharactersState/Vampire/VampireAttackState.cs
@@ -0,0 +1,33 @@
+using System;
+using Godot;
+public partial class VampireAttackState : VampireStateBase
+{
+  [Export] private RayCastNPCDetector _detector;
+
+  public override void _Ready()
+  {
+    base._Ready();
+    _vampire.AnimationPlayer.AnimationFinished += OnAnimationFinished;
+  }
+
+
+  public override void Start()
+  {
+    //_vampire.AnimationPlayer.Play(AnimationNameVampire.Attacking);
+    NPC npc = _detector.NPCDetected;
+    npc.EmitIamOnAttackSignal();
+    _vampire.GlobalPosition = npc.GlobalPosition;
+    StateMachine.ChangeState(VampireStateNames.Idle);
+  }
+
+  public override void End()
+  {
+
+  }
+
+
+  private void OnAnimationFinished(StringName animationName)
+  {
+    StateMachine.ChangeState(VampireStateNames.Idle);
+  }
+}
