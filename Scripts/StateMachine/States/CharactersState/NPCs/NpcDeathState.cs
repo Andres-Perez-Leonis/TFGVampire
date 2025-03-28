c7d5780521379c14451899ff49a0ef6f7fe70622
AndresPerez <apleonis@gmail.com>
2025-03-28 22:12:41 +0100

    NPC Death State Added

---
+++ Scripts/StateMachine/States/CharactersState/NPCs/NpcDeathState.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class NpcDeathState : NpcStateBase
+{
+    public override void Start()
+    {
+        base.Start();
+		//_npc.AnimationPlayer.Play(AnimationNameNPC.Death);
+		_npc.IsDeath = true;
+		_npc.SetCollisionLayerValue(4, true); // Set the entity to the Corpse Layer
+		_npc.SetCollisionLayerValue(3, false); // Unset the entity to the NPC Layer
+    }
+
+}
