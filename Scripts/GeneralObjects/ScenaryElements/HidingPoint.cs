072e38f5f305b942740f17582e7d85d5585e45ac
AndresPerez <apleonis@gmail.com>
2025-04-18 19:46:59 +0200

    Do a abstract class. Interact Abstract Method Added

--- Scripts/GeneralObjects/ScenaryElements/HidingPoint.cs
+++ Scripts/GeneralObjects/ScenaryElements/HidingPoint.cs
@@ -1,11 +1,16 @@
 using Godot;
 
-public partial class HidingPoint : Area2D
+public abstract partial class HidingPoint : Area2D
 {
     [Export] private AnimatedSprite2D _animator;
 
 
     [Export] private UserInterface _interface;
+    
+    [Export] private float _defaultFindHiddenNodeProbability = 0.5f;
+    [Export] protected float _currentFindHiddenNodeProbability;
+
+    private Node _hidenNode;
 
 
     private bool _nodeInArea = false;
@@ -13,7 +18,9 @@ public partial class HidingPoint : Area2D
     public override void _Ready()
     {
         base._Ready();
+        _currentFindHiddenNodeProbability =_defaultFindHiddenNodeProbability;
     }
+    public abstract int Interact();
 
     public override void _PhysicsProcess(double delta)
     {
@@ -22,9 +29,13 @@ public partial class HidingPoint : Area2D
         _interface.Fading(_nodeInArea, delta);
     }
 
-    private Node _hidenNode;
 
     public Node HidenNode  { get => _hidenNode; set=> _hidenNode = value; }
 
     public AnimatedSprite2D Animator { get => _animator; }
+
+    public float CurrentFindHiddenNodeProbability {
+        get => _currentFindHiddenNodeProbability;
+        set => _currentFindHiddenNodeProbability = Mathf.Min(1, value);
+    }
 }
