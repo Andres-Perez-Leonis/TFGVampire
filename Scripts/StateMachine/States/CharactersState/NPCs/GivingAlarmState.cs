0a4dea25555fd2bd2229393e8793aa18748321b7
AndresPerez <apleonis@gmail.com>
2025-04-19 13:58:22 +0200

    Giving Alarm State Added

---
+++ Scripts/StateMachine/States/CharactersState/NPCs/GivingAlarmState.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+public partial class GivingAlarmState : MovingStateBase
+{
+    [Export] private MarkerPathSwitch _guardPoint;
+
+
+    public override void Start()
+    {
+        base.Start();
+        _npc.AnimationPlayer.Play(AnimationNameNPC.Fleeing);
+        _npc.Speed *= 2;
+        _npc.Destination = _guardPoint;
+    }
+
+
+    public override void OnPhysicsProcess(double delta)
+    {
+        base.OnPhysicsProcess(delta);
+    }
+
+    protected override void InMyDestination()
+    {
+        // GUARDS ATTACK
+    }
+
+}
