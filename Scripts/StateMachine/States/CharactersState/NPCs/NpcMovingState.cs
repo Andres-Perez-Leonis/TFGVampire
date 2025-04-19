bf74ba9ba2b73c19b900bea0bba5c2dbd1fc37fc
AndresPerez <apleonis@gmail.com>
2025-04-19 13:56:15 +0200

    Change his inheritance class

--- Scripts/StateMachine/States/CharactersState/NPCs/NpcMovingState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/NpcMovingState.cs
@@ -1,24 +1,9 @@
 using Godot;
 
-public partial class NpcMovingState : NpcStateBase
+public partial class NpcMovingState : MovingStateBase
 {
-    /***
-     * The Area2D used to detect intersections for path switching.
-     */
-    [Export]
-    protected Area2D _interconectionDetector;
 
-    [Export] protected HidingPointDetector _hidingPointDetector;
-
-    /***
-     * The MarkerPathSwitch used to change the NPC's path when needed.
-     */
-    private MarkerPathSwitch _markerPathSwitch;
-
-    /***
-     * Boolean flag to determine the direction of the NPC's movement along the path.
-     */
-    private bool _progressInDown = false;
+    [Export] private HidingPointDetector _hidingPointDetector;
 
     /***
      * Called when the node enters the scene tree for the first time.
@@ -27,9 +12,6 @@ public partial class NpcMovingState : NpcStateBase
     public override void _Ready()
     {
         base._Ready();
-        _interconectionDetector.AreaEntered += MarkerSwitchDetected;
-        _pathFollow.OnChangePath += CheckOrientation;
-        _pathFollow.InMyDestination += InMyDestination;
         
         GetNode<Area2D>("../../VisionCone2D/VisionConeArea").BodyEntered += OnBodyEnteredInVisionCone;
     }
@@ -40,7 +22,7 @@ public partial class NpcMovingState : NpcStateBase
         if(node is not NPC) return;
         if(((NPC) node).IsHide) return;
         GD.Print("He visto un cadaver");
-        StateMachine.ChangeState(NpcStateNames.GivingAlarmRunning);
+        StateMachine.ChangeState(NpcStateNames.GivingAlarm);
     }
 
     /***
@@ -50,7 +32,6 @@ public partial class NpcMovingState : NpcStateBase
      */
     public override void OnPhysicsProcess(double delta)
     {
-        base.OnPhysicsProcess(delta);
 
         int nodeFounded = 0;
 
@@ -59,93 +40,18 @@ public partial class NpcMovingState : NpcStateBase
         
         if(_hidingPointDetector.HidingPoint != null) nodeFounded = _hidingPointDetector.HidingPoint.Interact();
         if(nodeFounded != 0) StateMachine.ChangeState( nodeFounded == 1 ? NpcStateNames.GivingAlarmRunning : NpcStateNames.Weird); 
-        // Calculate the progress in the path
-        float progress = (float)delta * _npc.Speed;
-        _pathFollow.ProgressRatio += (_progressInDown) ? -progress : progress;
-        //GD.Print("Proogress Ratio: " + _pathFollow.ProgressRatio);
 
-        // Check if the NPC has reached the end or start of the path to trigger the next path
-        if (_pathFollow.ProgressRatio == 1 || _pathFollow.ProgressRatio == 0) 
-            NextPath();
+
+        base.OnPhysicsProcess(delta);
 
             
     }
 
-
-
-    /***
-     * Changes the NPC's path when the current path is completed.
-     * Ensures the path interconnector exists before proceeding.
-     */
-    private void NextPath()
-    {
-        if (_markerPathSwitch == null)
-        {
-            GD.Print("Error: Path Interconnector Undetected");
-            return;
-        }
-        _markerPathSwitch.ChangeTheNPCRoute(_pathFollow, _npc.CurrentAction);
-    }
-
-    /***
-     * Checks and updates the NPC's orientation based on the current and next path's direction.
-     * This adjusts the NPC's scale to reflect the correct orientation for movement.
-     * @param intialPointOfCurrentPath The starting point of the current path.
-     * @param finalPointOfNextPath The endpoint of the next path.
-     */
-    private void CheckOrientation(Vector2 finalPointOfNextPath)
-    {
-        float distanceFinal = _npc.GlobalPosition.DistanceTo(finalPointOfNextPath);
-        //GD.Print($"Distacia desde {_npc.GlobalPosition} al punto final {finalPointOfNextPath}: {distanceFinal}");
-
-
-        float angle, direction;
-
-        angle = _npc.GlobalPosition.AngleToPoint(_npc.CurrentAction.GlobalPosition);
-        direction = Mathf.Sign(Mathf.Cos(angle));
-
-        float scaleFactor = Mathf.Abs(_npc.Scale.X);
-        if (_npc.Scale.X != direction * scaleFactor)
-        {
-            rotate();
-        }
-
-        _progressInDown = distanceFinal < 50;
-        //GD.Print("InDown: " + _progressInDown);
-        _pathFollow.ProgressRatio = _progressInDown ? 1 : 0;
-        //GD.Print("Proogress Ratio on Change: " + _pathFollow.ProgressRatio);
-    }
-
-    /***
-     * Rotates the NPC to face the opposite direction by flipping its scale on the X-axis.
-     */
-    public void rotate()
-    {
-        _npc.Scale = new Vector2(_npc.Scale.X * -1, _npc.Scale.Y);
-    }
-
-    /***
-     * Called when the interconnection detector (Area2D) detects a new marker path.
-     * If the marker path belongs to the NPC's work location, the state is changed to Working.
-     * @param area2D The Area2D that triggered the event.
-     */
-    private void MarkerSwitchDetected(Area2D area2D)
-    {
-        MarkerPathSwitch markerPath = area2D.GetParent<MarkerPathSwitch>();
-        if (markerPath == _npc.WorkPlace && _npc.WorkPlace == _npc.CurrentAction)
-            StateMachine.ChangeState(NpcStateNames.Working);
-        
-        _markerPathSwitch = markerPath;
-    }
-
-    /***
-     * Placeholder method for actions when the NPC reaches its destination.
-     * Transitions to connected states such as Working, Keeping Object, Idle, or Talking.
-     */
-    private void InMyDestination()
+    protected override void InMyDestination()
     {
         // Transition to connected states based on destination
         _npc.NextTask();
         //StateMachine.ChangeState(NpcStateNames.Idle);
     }
+
 }
