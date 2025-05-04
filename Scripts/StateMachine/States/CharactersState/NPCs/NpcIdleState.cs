0ee551c2747c737eb692af2069ce4d482df3b0d6
AndresPerez <apleonis@gmail.com>
2025-05-04 13:47:28 +0200

    Change where the animation process is called

--- Scripts/StateMachine/States/CharactersState/NPCs/NpcIdleState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/NpcIdleState.cs
@@ -2,10 +2,15 @@ using Godot;
 
 public partial class NpcIdleState : NpcStateBase
 {
-    
+    public override void Start()
+    {
+        base.Start();
+        //_npc.AnimationPlayer.Play(AnimationNameNPC.Idle);
+    }
+
+
     public override void OnPhysicsProcess(double delta)
     {
-        //_npc.AnimationPlayer.Play(AnimationNameNPC.Idle);
         if(_npc.PathFollow.LastPassMarker != _npc.CurrentAction) StateMachine.ChangeState(NpcStateNames.Moving);
         GD.Print($"Soy {_npc.Name} estoy buscando {_npc.CurrentAction.Name} y vengo de {_npc.PathFollow.LastPassMarker.Name}");
     }
