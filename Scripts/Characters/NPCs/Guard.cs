01b64c369f35f5aece7327c369b809e6c64b1064
AndresPerez <apleonis@gmail.com>
2025-05-13 12:39:19 +0200

    Debug and NotifyCorpseFound Signal Reconfigured

--- Scripts/Characters/NPCs/Guard.cs
+++ Scripts/Characters/NPCs/Guard.cs
@@ -4,10 +4,12 @@ public partial class Guard : NPC
 {
     private NPC _corpseToCheck;
 
-    [Signal] public delegate void NotifyCorpseFoundEventHandler(NPC corpse);
+    [Signal] public delegate void NotifyCorpseFoundEventHandler();
 
     public void EmitNotifyCorpseFoundSignal(NPC corpse) {
-        EmitSignal(SignalName.NotifyCorpseFound, corpse);
+        if(corpse == null) GD.Print("En la llamada al guardía se pierde el corpse");  // IS NOT NULL
+        _corpseToCheck = corpse;
+        EmitSignal(SignalName.NotifyCorpseFound);
     }
 
     public NPC CorpseToCheck { get => _corpseToCheck; set => _corpseToCheck = value; }
