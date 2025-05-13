f0b0b945d6cf6529ff2146de49ab8fc05319f907
AndresPerez <apleonis@gmail.com>
2025-05-13 12:36:05 +0200

    Corspe Point Gruop Added

--- Resources/Groups/NameGroups.cs
+++ Resources/Groups/NameGroups.cs
@@ -13,4 +13,5 @@ public partial class NameGroups : Resource
 	public const string InterconnectionRoute = "InterconnectionRoute";
 	public const string NPCsRoutes = "NPCsRoutes";
 	public const string GuardGroup = "Guards";
+	public const string CorpsePoint = "CorpsePoint";
 }
