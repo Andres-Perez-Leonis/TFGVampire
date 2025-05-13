3eb352cb4f35ea801cfd0cf77301dc07c24cd59a
AndresPerez <apleonis@gmail.com>
2025-05-13 12:47:38 +0200

    Line commented

--- Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardIdleState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardIdleState.cs
@@ -9,10 +9,7 @@ public partial class GuardIdleState : GuardStateBase
         _guard.NotifyCorpseFound += GoToCorpse;
     }
 
-    private void GoToCorpse(NPC corpse) {
-        
-        GD.Print("Me han llamado soy " + Name);
-        _guard.CorpseToCheck = corpse;
+    private void GoToCorpse() {
         //_guard.Destination = _corpseToCheck.PathFollow.LastPassMarker;
         StateMachine.ChangeState(NpcStateNames.Moving);
     }
