8f23953e5528e96c0ceb705f0bf7fb366f18c7ff
AndresPerez <apleonis@gmail.com>
2025-05-13 12:48:41 +0200

    InMyDestination Function done

--- Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardMovingState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardMovingState.cs
@@ -1,13 +1,47 @@
 using Godot;
 using System;
 
-public partial class GuardMovingState : GuardStateBase
+public partial class GuardMovingState : GuardMovingStateBase
 {
+    [Export] RayCast2D _corpseDetector;
+    
+    private bool _inTheSamePath = false;
+    public override void OnPhysicsProcess(double delta)
+    {
+        base.OnPhysicsProcess(delta);
+
+        bool nextToTheCorpse = _corpseDetector.IsColliding();
+        if(nextToTheCorpse && _inTheSamePath)
+            StateMachine.ChangeState(GuardStateNames.Drag);
+    }
+
     public override void Start()
     {
         base.Start();
-        _guard.Destination = _guard.CorpseToCheck.PathFollow.LastPassMarker;
+        CheckPath();
     }
 
+    private void CheckPath() {
+        _inTheSamePath = _guard.CorpseToCheck.PathFollow.GetParent<Path2D>() == _guard.PathFollow.GetParent<Path2D>();
+        if(_inTheSamePath)
+            InMyDestination();
+    }
+
+
+    protected override void NextPath()
+    {
+        base.NextPath();
+        CheckPath();
+    }
+
+    protected override void InMyDestination()
+    {
+        if(_inTheSamePath) return;
+        _inTheSamePath = true;
+        Path2D nextPath = _guard.CorpseToCheck.PathFollow.GetParent<Path2D>();
+        _guard.PathFollow.EmitOnChangePathSignal(nextPath.ToGlobal(nextPath.Curve.GetPointPosition(nextPath.Curve.PointCount - 1)));
+        _guard.PathFollow.GetParent<Path2D>().RemoveChild(_guard.PathFollow);
+        _guard.CorpseToCheck.PathFollow.GetParent<Path2D>().AddChild(_guard.PathFollow);
+    }
 
 }
