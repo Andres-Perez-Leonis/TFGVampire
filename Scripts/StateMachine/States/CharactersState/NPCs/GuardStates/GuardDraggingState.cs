dee5b3a17ddf7b9470ac4c13203aff5a25c29555
AndresPerez <apleonis@gmail.com>
2025-05-21 16:30:57 +0200

    Guard Can Dragging Corpses

--- Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardDraggingState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardDraggingState.cs
@@ -1,21 +1,35 @@
 using Godot;
-using System;
 
 public partial class GuardDraggingState : GuardMovingStateBase
 {
     private NPC _draggingCorpse;
 
     private MarkerPathSwitch _corpsesPoint;
-    [Export] private float _nextToGuardOffset = 0.015f;
+    [Export] private float _nextToGuardOffset = 20;
 
     public override void Start()
     {
-        base.Start();
+        //base.Start();
         _draggingCorpse = _guard.CorpseToCheck;
         _guard.Destination = _corpsesPoint;
-        GD.Print("Yo " + Owner.Name + " tengo un progreso " + _guard.PathFollow.ProgressRatio);
-        GD.Print("Yo " + "cadaver" + " tengo un progreso " + _draggingCorpse.PathFollow.ProgressRatio);
-       //GetTree().Quit();
+
+
+        //GD.Print("Yo " + Owner.Name + " tengo un progreso " + _guard.PathFollow.ProgressRatio);
+        //GD.Print("Yo " + "cadaver" + " tengo un progreso " + _draggingCorpse.PathFollow.ProgressRatio);
+        //GetTree().Quit();
+
+
+        _draggingCorpse.Scale /= _guard.Scale;
+        _draggingCorpse.Position = new Vector2(0, 0);
+        _draggingCorpse.PathFollow.GetParent<Path2D>().RemoveChild(_draggingCorpse.PathFollow);
+        _guard.AddChild(_draggingCorpse.PathFollow);
+        _draggingCorpse.PathFollow.GlobalPosition = new Vector2(_guard.PathFollow.GlobalPosition.X + _nextToGuardOffset, _guard.PathFollow.GlobalPosition.Y - 20);
+
+
+        //GD.Print("Posicion Guardia: " + _guard.GlobalPosition);
+        //GD.Print("Posicion Cadaver: " + _draggingCorpse.GlobalPosition);
+        //GD.Print("Posicion local guardia: " + _guard.Position);
+        //GD.Print("Posicion local Cadaver: " + _draggingCorpse.Position);
     }
 
     public override void _Ready()
@@ -27,23 +41,17 @@ public partial class GuardDraggingState : GuardMovingStateBase
 
     protected override void InMyDestination()
     {
-
+        GD.Print("El del dragging se ejecuta");
+        //draggingCorpse.PathFollow.QueueFree();
+        //_draggingCorpse.QueueFree();
     }
 
     public override void OnPhysicsProcess(double delta)
     {
         base.OnPhysicsProcess(delta);
-        _draggingCorpse.PathFollow.ProgressRatio = _guard.PathFollow.ProgressRatio - _nextToGuardOffset;
+        //GD.Print("Yo " + Owner.Name + " tengo un progreso " + _guard.PathFollow.ProgressRatio);
+        //GD.Print("Yo " + "cadaver" + " tengo un progreso " + _draggingCorpse.PathFollow.ProgressRatio);
 
-        if(_draggingCorpse.PathFollow.ProgressRatio == 1 || _draggingCorpse.PathFollow.ProgressRatio == 0) {
-            _draggingCorpse.PathFollow.GetParent<Path2D>().RemoveChild(_draggingCorpse.PathFollow);
-            _guard.PathFollow.GetParent<Path2D>().AddChild(_draggingCorpse.PathFollow);
-        }
-
-        
-        GD.Print("Yo " + Owner.Name + " tengo un progreso " + _guard.PathFollow.ProgressRatio);
-        GD.Print("Yo " + "cadaver" + " tengo un progreso " + _draggingCorpse.PathFollow.ProgressRatio);
-            
     }
 
 }
