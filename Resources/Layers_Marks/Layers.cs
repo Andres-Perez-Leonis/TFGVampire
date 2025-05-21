c24d3b5122929e2f87d111399a05d69527bb6ff4
AndresPerez <apleonis@gmail.com>
2025-05-21 14:10:17 +0200

    Layers And Masks Resource for reference their values added

---
+++ Resources/Layers_Marks/Layers.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class IntLayerMarks : Resource
+{
+	public const int Floor = 1;
+	public const int Vampire = 2;
+	public const int NPC = 4;
+	public const int Corpse = 8;
+	public const int Route = 16;
+	public const int HidingPoint = 32;
+	public const int Wall = 64;
+	public const int Area = 128;
+
+}
