d802b0033840af8d03f0f2327c34830a78886c2a
AndresPerez <apleonis@gmail.com>
2025-05-22 13:21:21 +0200

    Change the Signal which will be emitted

--- Scripts/StateMachine/States/CharactersState/NPCs/GivingAlarmRunningState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GivingAlarmRunningState.cs
@@ -6,44 +6,38 @@ public partial class GivingAlarmRunningState : NPCMovingStateBase
 {
     //[Signal] public delegate void CorpseFoundedEventHandler(Node2D node2D);
 
-    private NPC _corpseFounded;
+    [Export] private int _guardWarnCount = 2;
     public override void Start()
     {
-        base.Start();
-        if (_corpseFounded == null) GetTree().Quit();
         //_npc.AnimationPlayer.Play(AnimationNameNPC.Shout);
         List<Node> guards = GetTree().GetNodesInGroup(NameGroups.GuardGroup).ToList();
-        List<Node2D> nearestGuards = guards.OfType<Node2D>().OrderBy(guard => _npc.GlobalPosition.DistanceSquaredTo(guard.GlobalPosition)).Take(2).ToList();
+
+        List<Node2D> nearestGuards = guards.OfType<Node2D>()
+            .OrderBy(guard => _npc.GlobalPosition.DistanceSquaredTo(guard.GlobalPosition))
+            .Take(_guardWarnCount).ToList();
+        GD.Print("Progress: " + _npc.PathFollow.ProgressRatio);
+
         foreach (Guard guard in nearestGuards)
         {
-            guard.EmitNotifyCorpseFoundSignal(_corpseFounded);
+            guard.EmitVampireDetectedSignal();
         }
-
+        rotate();
         _npc.Speed += 0.1f;
-        _progressInDown = !_progressInDown;
+        //_progressInDown = !_progressInDown;
         _npc.ToHome();
+    }
+
+    public override void OnPhysicsProcess(double delta)
+    {
+        base.OnPhysicsProcess(delta);
+        
+        GD.Print("Progress: " + _npc.PathFollow.ProgressRatio);
     }
 
     public override void End()
     {
         base.End();
         _npc.Speed -= 0.1f;
-    }
-
-
-    public override void _Ready()
-    {
-        base._Ready();
-        GetNode<Area2D>("../../CorpseDetector/VisionConeArea").BodyEntered += OnCorpseFounded;
-    }
-
-    private void OnCorpseFounded(Node2D corpse) {
-        _corpseFounded = (NPC) corpse;
-    }
-
-    public override void OnPhysicsProcess(double delta)
-    {
-        base.OnPhysicsProcess(delta);
     }
 
     protected override void InMyDestination()
