99829ce5a47e924058bd497371d15726ca2109e1
AndresPerez <apleonis@gmail.com>
2025-05-31 19:11:22 +0200

    Guard can approach to Vampire

--- Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardAttackState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardAttackState.cs
@@ -1,11 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
-public partial class GuardAttackState : GuardStateBase
+public partial class GuardAttackState : GuardMovingStateBase
 {
-	public override void Start()
+
+	private static Vampire _vampire;
+
+	public override void _Ready()
 	{
-		base.Start();
-		
+		base._Ready();
+		CallDeferred("_readyCallDefered");
     }
 
+	private void _readyCallDefered()
+	{
+		_vampire = (Vampire)GetTree().GetFirstNodeInGroup(NameGroups.Vampire);
+	}
+
+
+	public override void Start()
+	{
+		GetNode<Area2D>("../../VampireDetector/VisionConeArea").BodyEntered += _nearVampire;
+		List<Node> markerPathSwitch = GetTree().GetNodesInGroup(NameGroups.InterconnectionRoute).ToList();
+
+		MarkerPathSwitch nearestMarker = markerPathSwitch.OfType<MarkerPathSwitch>()
+			.OrderBy(marker => _vampire.GlobalPosition.DistanceSquaredTo(marker.GlobalPosition))
+			.First();
+
+		_guard.Destination = nearestMarker;
+		CheckOrientation(nearestMarker.GlobalPosition);
+	}
+
+	private void _nearVampire(Node2D node2D)
+	{
+		// GAME OVER
+		GetTree().Quit();
+	}
+
+    protected override void InMyDestination()
+    {
+    }
 }
