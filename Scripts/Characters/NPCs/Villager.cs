11e5263badc8d5cd1647ccf73b3484efc0abf014
AndresPerez <apleonis@gmail.com>
2025-06-02 18:54:27 +0200

    Attribute Personality Added

--- Scripts/Characters/NPCs/Villager.cs
+++ Scripts/Characters/NPCs/Villager.cs
@@ -1,11 +1,27 @@
+using System;
 using Godot;
 
 public partial class Villager : NPC
 {
 
     private bool _availableToTalk = true;
+    private Personality _personality;
     [Export] private SuspiciousSystem _suspiciousSystem;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        Random random = new Random(GetHashCode());
+        _personality = new Personality(
+            easyInfluenced: random.Next(0, 1) == 1,
+            believeInMisticysm: random.Next(0, 1) == 1,
+            gossipy: random.Next(0, 1) == 1,
+            brave: random.Next(0, 1) == 1,
+            prudent: random.Next(0, 1) == 1
+        );  
+    }
+    
+    public Personality Personality { get => _personality; }
     public SuspiciousSystem SuspiciousSystem { get => _suspiciousSystem; set => _suspiciousSystem = value; }
     public bool AvailableToTalk { get => _availableToTalk; set => _availableToTalk = value; }
 }
