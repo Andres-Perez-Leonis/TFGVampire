3c337f3cbc496f3ac9b06d08bac52a2f2e10c20c
AndresPerez <apleonis@gmail.com>
2025-06-02 18:50:46 +0200

    Change Attribute's Resource to Public

--- Resources/NPCPresonality/Personality.cs
+++ Resources/NPCPresonality/Personality.cs
@@ -3,22 +3,22 @@ using System;
 
 public partial class Personality : Resource
 {
-    [Export] public bool _easyInfluenced { get; set; }
-    [Export] public bool _believeInMisticysm { get; set; }
-    [Export] public bool _gossipy { get; set; }
-    [Export] public bool _inMadness { get; set; }
-    [Export] public bool _brave { get; set; }
-    [Export] public bool _prudent { get; set; }
+    public bool EasyInfluenced { get; set; }
+    public bool BelieveInMisticysm { get; set; }
+    public bool Gossipy { get; set; }
+    public bool InMadness { get; set; }
+    public bool Brave { get; set; }
+    public bool Prudent { get; set; }
     public Personality() : this(false, false, false, false, false, false) { }
 
-    public Personality(bool easyInfluenced, bool believeInMisticysm, bool gossipy, bool inMadness, bool brave, bool prudent)
+    public Personality(bool easyInfluenced, bool believeInMisticysm, bool gossipy, bool brave, bool prudent,bool inMadness = false)
     {
-        _easyInfluenced = easyInfluenced;
-        _believeInMisticysm = believeInMisticysm;
-        _gossipy = gossipy;
-        _inMadness = inMadness;
-        _brave = brave;
-        _prudent = prudent;
+        EasyInfluenced = easyInfluenced;
+        BelieveInMisticysm = believeInMisticysm;
+        Gossipy = gossipy;
+        InMadness = inMadness;
+        Brave = brave;
+        Prudent = prudent;
     }
 
 }
