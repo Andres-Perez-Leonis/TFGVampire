245b8bc50770e12f831c822f8e6f5dd67f17fa39
AndresPerez <apleonis@gmail.com>
2025-06-09 13:06:23 +0200

    Change Parent Class to NPCStateBase

--- Scripts/StateMachine/States/CharactersState/NPCs/NpcIdleState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/NpcIdleState.cs
@@ -1,18 +1,16 @@
 using Godot;
 
-public partial class NpcIdleState : VillagerStateBase
+public partial class NpcIdleState : NpcStateBase
 {
     public override void Start()
     {
         base.Start();
         //_npc.AnimationPlayer.Play(AnimationNameNPC.Idle);
-        if(_npc.GetNode<VillagerDetector>("VillagerDetector").Villager != null) StateMachine.ChangeState(NpcStateNames.Talking);
+        string nextState = NpcStateNames.Moving;
+        if (_npc.GetNode<VillagerDetector>("VillagerDetector").Villager != null) nextState = NpcStateNames.Talking;
+        //else if (_npc.PathFollow.LastPassMarker != _npc.CurrentAction) StateMachine.ChangeState(NpcStateNames.Moving);
+        StateMachine.ChangeState(nextState);
     }
 
 
-    public override void OnPhysicsProcess(double delta)
-    {
-        if(_npc.PathFollow.LastPassMarker != _npc.CurrentAction) StateMachine.ChangeState(NpcStateNames.Moving);
-        GD.Print($"Soy {_npc.Name} estoy buscando {_npc.CurrentAction.Name} y vengo de {_npc.PathFollow.LastPassMarker.Name}");
-    }
 }
