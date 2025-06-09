0faa2db4717c5ecf0b92e2bea78681e1e01dd8d5
AndresPerez <apleonis@gmail.com>
2025-06-09 13:09:20 +0200

    Mediator and Meditor Manager Reference Added and Villager can talk each other

--- Scripts/StateMachine/States/CharactersState/NPCs/VillagerTalkingState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/VillagerTalkingState.cs
@@ -7,49 +7,42 @@ public partial class VillagerTalkingState : VillagerStateBase
 
 	//[Signal] public delegate void TalkRequestEventHandler(Villager sender, Villager recipient);
 
-	private static Mediator _mediator;
+	private static MediatorManager MediatorManager;
 	private Villager _villagerToTalk;
 	[Export] private VillagerDetector _detector;
+
+	private Mediator _mediator;
 
 	public override void _Ready()
 	{
 		base._Ready();
-		_mediator = GetTree().Root.GetNode<Mediator>("Mediator");
+		MediatorManager = GetTree().Root.GetNode<MediatorManager>("MainScene/MediatorManager");
 	}
 
 	public override void Start()
 	{
 		base.Start();
-		
+
 		_villagerToTalk = _detector.Villager;
-		VillagerTalkingState villagerTalkingState = _villagerToTalk.GetNode	<VillagerTalkingState>("StateMachine/TalkingState");
-		_mediator.TalkRequest(this, villagerTalkingState);
+		VillagerTalkingState villagerTalkingState = _villagerToTalk.GetNode<VillagerTalkingState>("StateMachine/TalkingState");
+		MediatorManager.TalkRequest(this, villagerTalkingState);
 	}
 
-
-
-
-	/*
-		public void EmitTalkRequest(Villager recipient)
-		{
-			EmitSignal(SignalName.TalkRequest, this, recipient);
-		}
-	*/
-
-	[Signal] public delegate void ISendSuspisionEventHandler(Villager[] villagersInSuspech);
 	public void InitConversation(Villager villager)
 	{
-		if (_villager.Personality.Gossipy)
-		{
-			Villager[] villagersInSuspech = _villager.SuspiciousSystem.VillagerInSuspech(3);
-			EmitSignal(SignalName.ISendSuspision, villagersInSuspech);
-		}
-
+		List<EntitySuspechData> villagersInSuspech = _villager.SuspicionSystem.EntityInSuspech(3, _villager);
+		_mediator.SendInfo(this, villagersInSuspech);
 	}
 
-	public void HeardSuspisions(Villager[] villagersInSuspech, bool thinkIsAVampire)
+	public void HeardSuspisions(List<EntitySuspechData> villagersInSuspech, Villager whoTellMe)
 	{
-		
+		_villager.SuspicionSystem.AnalizeSupisions(villagersInSuspech, whoTellMe);
+	}
+
+	public void FinishConversation()
+	{
+		_detector.ResetDetector();
+		StateMachine.ChangeState(NpcStateNames.Idle);
 	}
 
 	public void RejectConversation(Villager villager)
