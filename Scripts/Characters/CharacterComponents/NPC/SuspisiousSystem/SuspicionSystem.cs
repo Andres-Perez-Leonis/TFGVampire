cca80a5e23ccf90db9102ab94a5a90b5317cd2b4
AndresPerez <apleonis@gmail.com>
2025-06-09 13:12:25 +0200

    Suspision System Added and configured

---
+++ Scripts/Characters/CharacterComponents/NPC/SuspisiousSystem/SuspicionSystem.cs
@@ -0,0 +1,114 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class SuspicionSystem : Node
+{
+
+	private List<EntitySuspechData> _suspechData = new();
+	private Dictionary<Entity, EntitySuspechData> _dicSuspechData;
+
+	[Export] private int _thresholdSuspision = 90;
+	[Export] private int _thresholdTalkAGuard = 90;
+
+	[Export] private int _reductionSuspisionAmount = 10;
+	[Export] private int _increaseSuspisionAmount = 10;
+
+	private Villager _myVillager;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_myVillager = Owner as Villager;
+		_myVillager.InMadness += InMadness;
+		CallDeferred("CallDeferredReady");
+	}
+
+
+	private void CallDeferredReady()
+	{
+		VillagerListCreator creator = GetTree().Root.GetNode<VillagerListCreator>("MainScene/VillagerListCreator");
+		List<Villager> villagers = creator.ListOfVillagers;
+
+		foreach (Entity entity in villagers)
+		{
+			_suspechData.Add(new(entity, 10));
+		}
+		_suspechData.Add(new(GetTree().GetFirstNodeInGroup(NameGroups.Vampire) as Entity)); // Vampire
+		_dicSuspechData = _suspechData.ToDictionary(e => e.Entity);
+		ShowSuspisions();
+
+	}
+
+	public List<EntitySuspechData> EntityInSuspech(int entityAmountToTell)
+	{
+		return (_myVillager.Personality.Gossipy) ? _suspechData.Where(e => e.AmountOfSuspusion > 0).Take(entityAmountToTell).ToList() : new();
+	}
+
+	public List<EntitySuspechData> EntityInSuspech(int entityAmountToTell, Villager whoItelling)
+	{
+		if (!_myVillager.Personality.Gossipy) return new();
+		List<EntitySuspechData> entities =_suspechData.Where(e => e.AmountOfSuspusion > 0).Take(entityAmountToTell).ToList();
+		if (_myVillager.Personality.Prudent)
+			entities.Remove(_dicSuspechData[whoItelling]);
+		return entities;
+	}
+
+	public List<EntitySuspechData> EntityInSuspech()
+	{
+		return _suspechData.Where(e => e.AmountOfSuspusion > 0).Take(3).ToList();
+	}
+
+	public void AnalizeSupisions(List<EntitySuspechData> entities, Entity whoSaidMeThat)
+	{
+		//Comprobamos las sospechas del otro NPC
+		if (!_myVillager.Personality.EasyInfluenced) return;
+
+		bool heThinkItsMe = false;
+
+		//Dictionary<Entity, EntitySuspechData> suspechData = _suspechData.ToDictionary(s => s.Entity);
+		foreach (EntitySuspechData entityData in entities)
+		{
+			heThinkItsMe = (entityData.Entity == _myVillager);
+			_dicSuspechData[entityData.Entity].AmountOfSuspusion += _increaseSuspisionAmount;
+		}
+
+		if (heThinkItsMe) _dicSuspechData[whoSaidMeThat].AmountOfSuspusion += _increaseSuspisionAmount;
+
+		HashSet<Entity> hashEntity = new HashSet<Entity>(EntityInSuspech(entities.Count).Select(s => s.Entity));
+		int matches = entities.Count(e => hashEntity.Contains(e.Entity));
+
+		_dicSuspechData[whoSaidMeThat].AmountOfSuspusion -= _reductionSuspisionAmount * matches;
+
+		_suspechData.Sort((a, b) => b.AmountOfSuspusion.CompareTo(a.AmountOfSuspusion));
+		ShowSuspisions();
+	}
+
+	private void ShowSuspisions()
+	{
+		GD.Print("Lista de sospechos");
+		for (int i = 0; i < _suspechData.Count; i++)
+		{
+			GD.Print(i + ". " + _suspechData[i].Entity.Name + " : " + _suspechData[i].AmountOfSuspusion);
+		}
+	}
+
+	public void InMadness()
+	{
+		List<EntitySuspechData> entities = _suspechData.Where(e => e.AmountOfSuspusion < 100).ToList();
+		for (int i = 0; i < entities.Count; i++)
+		{
+			entities[i].AmountOfSuspusion = 100;
+		}
+	}
+
+
+
+	public bool iSuspechOf(Entity entity)
+	{
+		return _thresholdSuspision < _dicSuspechData[entity].AmountOfSuspusion;
+	}
+	
+
+}
