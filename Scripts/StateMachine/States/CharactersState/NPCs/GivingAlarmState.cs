bb450c0889ef233e2cdae008e3ffab1b3ee2ae37
AndresPerez <apleonis@gmail.com>
2025-06-14 20:19:40 +0200

    Conditional to detect if the Guard has been alerted Added

--- Scripts/StateMachine/States/CharactersState/NPCs/GivingAlarmState.cs
+++ Scripts/StateMachine/States/CharactersState/NPCs/GivingAlarmState.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
-public partial class GivingAlarmState : NpcStateBase
+public partial class GivingAlarmState : VillagerStateBase
 {
     //[Signal] public delegate void CorpseFoundedEventHandler(Node2D node2D);
-
+    [Export] private VillagerDetector _detector;
     private NPC _corpseFounded;
     public override void Start()
     {
@@ -13,9 +13,17 @@ public partial class GivingAlarmState : NpcStateBase
         //_npc.AnimationPlayer.Play(AnimationNameNPC.Shout);
         List<Node> guards = GetTree().GetNodesInGroup(NameGroups.GuardGroup).ToList();
         List<Node2D> nearestGuards = guards.OfType<Node2D>().OrderBy(guard => _npc.GlobalPosition.DistanceSquaredTo(guard.GlobalPosition)).Take(2).ToList();
-        foreach(Guard guard in nearestGuards) {
-            guard.EmitNotifyCorpseFoundSignal(_corpseFounded);
+        foreach (Guard guard in nearestGuards)
+        {
+            guard.ReportCorpseFound(_corpseFounded);
         }
+
+        if (_detector.LastVillagerExposed == null)
+        {
+            GD.Print("Hubo un error y se ha perdido la referencia del ");
+        }
+
+        _villager.SuspicionSystem.IncreaseSuspision(_detector.LastVillagerExposed);
     }
 
     public override void _Ready()
