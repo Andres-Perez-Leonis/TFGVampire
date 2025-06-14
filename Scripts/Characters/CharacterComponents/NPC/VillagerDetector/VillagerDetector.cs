f49def031fb4f1f418ac1a26c06a16d2324a7199
AndresPerez <apleonis@gmail.com>
2025-06-14 20:14:37 +0200

    Last Entity Expoxed Attribute Added

--- Scripts/Characters/CharacterComponents/NPC/VillagerDetector/VillagerDetector.cs
+++ Scripts/Characters/CharacterComponents/NPC/VillagerDetector/VillagerDetector.cs
@@ -4,11 +4,13 @@ using System;
 public partial class VillagerDetector : Area2D
 {
 	private Villager _villager;
+	private Entity _lastEntityExposed;
 
 	public override void _Ready()
 	{
 		base._Ready();
 		BodyEntered += OnVillagerDetected;
+		BodyEntered += OnEntityDetected;
 	}
 
 	private void OnVillagerDetected(Node2D node)
@@ -16,6 +18,13 @@ public partial class VillagerDetector : Area2D
 		bool isAVillager = node is Villager;
 		if (!isAVillager) return;
 		_villager = (Villager)node;
+	}
+
+	private void OnEntityDetected(Node2D node)
+	{
+		bool isEntityNotGuard = node is Entity && node is not Guard;
+		if (!isEntityNotGuard) return;
+		_lastEntityExposed = node as Entity;
 	}
 
 	public void ResetDetector()
@@ -28,5 +37,7 @@ public partial class VillagerDetector : Area2D
 	{
 		get => _villager;
 	}
+
+	public Entity LastVillagerExposed { get => _lastEntityExposed; }
 
 }
