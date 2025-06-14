6d40a64bb57aeb7f9c5dc532d663497811aad541
AndresPerez <apleonis@gmail.com>
2025-06-14 20:17:39 +0200

    Notify me a Suspision Signal Added and Change Methods Names

--- Scripts/Characters/NPCs/Guard.cs
+++ Scripts/Characters/NPCs/Guard.cs
@@ -4,21 +4,46 @@ public partial class Guard : NPC
 {
     private NPC _corpseToCheck;
 
+    private Entity _entityInSuspech;
+
+    private bool _hasBeenAlerted  = false;
+
     [Signal] public delegate void NotifyCorpseFoundEventHandler();
+    [Signal] public delegate void NotifySuspisionEventHandler();
     [Signal] public delegate void VampireDetectedEventHandler();
 
-    public void EmitVampireDetectedSignal()
+    public void ReportVampireDetected()
     {
+        _hasBeenAlerted = true;
         EmitSignal(SignalName.VampireDetected);
     }
 
-
-    public void EmitNotifyCorpseFoundSignal(NPC corpse)
+    public void ReportSuspicion(Entity entity)
     {
-        if (corpse == null) GD.Print("En la llamada al guardía se pierde el corpse");  // IS NOT NULL
+        if (entity == null)
+        {
+            GD.Print("[Guard] Error: El villager sospechoso es nulo");
+            return;
+        }
+        _hasBeenAlerted = true;
+        _entityInSuspech = entity;
+        EmitSignal(SignalName.NotifySuspision);
+    }
+
+
+    public void ReportCorpseFound(NPC corpse)
+    {
+        if (corpse == null)
+        {
+            GD.PrintErr("[Guard] Error: El NPC cadáver es nulo  ");
+            return;
+        }
+        _hasBeenAlerted = true;
         _corpseToCheck = corpse;
         EmitSignal(SignalName.NotifyCorpseFound);
     }
 
     public NPC CorpseToCheck { get => _corpseToCheck; set => _corpseToCheck = value; }
+    public Entity EntityInSuspech { get => _entityInSuspech; set => _entityInSuspech = value; }
+    public bool HasBeenAlerted { get => _hasBeenAlerted; set => _hasBeenAlerted = value; }
 }
