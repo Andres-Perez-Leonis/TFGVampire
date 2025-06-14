b2b2626d92caf4e30f7df690f61f28185c94a5a1
AndresPerez <apleonis@gmail.com>
2025-06-14 20:21:50 +0200

    Scared State Added

---
+++ Scripts/StateMachine/States/CharactersState/NPCs/ScaredState.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class ScaredState : VillagerStateBase
+{
+	public override void Start()
+	{
+		base.Start();
+		//_villager.AnimationPlayer();
+    }
+
+}
