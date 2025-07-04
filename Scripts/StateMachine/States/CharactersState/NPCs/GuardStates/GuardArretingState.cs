7cd7192f1e3e1a09fbf890b1e4df6baf8ef216c3
AndresPerez <apleonis@gmail.com>
2025-07-05 01:49:14 +0200

    Guard Arresting State Added

---
+++ Scripts/StateMachine/States/CharactersState/NPCs/GuardStates/GuardArretingState.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+public partial class GuardArretingState : GuardMovingStateBase
+{
+	public override void Start()
+	{
+		base.Start();
+		if (_guard.EntityInSuspech is Villager villager)
+		{
+			_guard.Destination = villager.PathFollow.LastPassMarker;
+			villager.StopRightThere();
+			return;
+		}
+
+		Vampire vamp = _guard.EntityInSuspech as Vampire;
+		List<MarkerPathSwitch> markers = GetTree().GetNodesInGroup(NameGroups.InterconnectionRoute).OfType<MarkerPathSwitch>().ToList();
+		MarkerPathSwitch nearestMarkerToVamp = markers.OrderBy(m => m.GlobalPosition.DistanceSquaredTo(vamp.GlobalPosition)).First();
+		_guard.Destination = nearestMarkerToVamp;
+    }
+
+
+    protected override void InMyDestination()
+	{
+	}
+
+}
