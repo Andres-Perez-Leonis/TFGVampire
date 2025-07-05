37e8b218d033c083547b2db0bcb7220a48d1f7e2
AndresPerez <apleonis@gmail.com>
2025-07-05 19:21:42 +0200

    Attribute Setting Menu Access Modified

--- Scripts/MainMenu/Buttons/OptionsBtn/SettingsBtn.cs
+++ Scripts/MainMenu/Buttons/OptionsBtn/SettingsBtn.cs
@@ -3,14 +3,16 @@ using System;
 
 public partial class SettingsBtn : Button
 {
-	[Export] private Control _settingMenu;
+	private Control _settingMenu;
 	public override void _Ready()
 	{
+		_settingMenu = Owner.GetParent().GetNode<Control>("./SettingsMenu");
 		Pressed += _onPressed;
 	}
 
 	private void _onPressed()
 	{
 		_settingMenu.Visible = true;
+		((Control)Owner).Visible = false;
 	}
 }
