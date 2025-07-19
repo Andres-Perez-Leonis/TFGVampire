af402664e1ad2f1eb87495e22b5cc6e686eb15a4
AndresPerez <apleonis@gmail.com>
2025-07-19 12:29:05 +0200

    Resource for Main Menu Animation's Names Added

---
+++ Resources/AnimationResources/MainMenuAnimationName.cs
@@ -0,0 +1,13 @@
+using Godot;
+/**
+	Class for keep Animation names for Vampire
+
+	- To avoid the magic string
+
+*/ 
+
+public partial class MainMenuAnimationName : Resource
+{
+	public const string WaitingToStart = "Waiting_To_Start";
+	public const string InitGame = "Init_Game";
+}
