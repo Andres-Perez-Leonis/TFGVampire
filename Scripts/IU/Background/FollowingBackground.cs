443e2f4454ed75e23f97f979aa363b2567025c03
AndresPerez <apleonis@gmail.com>
2025-07-23 01:55:06 +0200

    Proof

--- Scripts/IU/Background/FollowingBackground.cs
+++ Scripts/IU/Background/FollowingBackground.cs
@@ -3,17 +3,20 @@ using Godot;
 public partial class FollowingBackground : Sprite2D
 {
 	private Vampire _vampire;
-	private Vector2 _offset;
+	private int _offset = 2360;
 	public override void _Ready()
 	{
-		_offset = GlobalPosition;
-		GD.Print("[Fondo] Debbug: Offset: " + _offset);
+		//_offset = GlobalPosition;
+		//GD.Print("[Fondo] Debbug: Offset: " + _offset);
 		_vampire = GetTree().GetFirstNodeInGroup(NameGroups.Vampire) as Vampire;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		GlobalPosition = _vampire.GlobalPosition + _offset;
+		return;
+		Vector2 pos = _vampire.GlobalPosition;
+		pos.X += _offset;
+		GlobalPosition = new (pos.X, GlobalPosition.Y);
 		GD.Print("[Fondo] Debbug: Siguiendo al jugador: " + GlobalPosition);
 		GD.Print("[Fondo] Debbug: Posicion jugador: " + _vampire.GlobalPosition);
     }
