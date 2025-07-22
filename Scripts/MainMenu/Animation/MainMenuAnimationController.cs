f79f28ec46de50afa36653cea39b797be0fc213f
AndresPerez <apleonis@gmail.com>
2025-07-23 01:54:50 +0200

    Main Menu Animation Controller Added

--- Scripts/MainMenu/Animation/MainMenuAnimationController.cs
+++ Scripts/MainMenu/Animation/MainMenuAnimationController.cs
@@ -1,14 +1,28 @@
 using Godot;
-using System;
 
 public partial class MainMenuAnimationController : AnimationPlayer
 {
+	[Export] private AudioStreamPlayer _song;
+	[Export] private AudioStreamPlayer _laugh;
 	public override void _Ready()
 	{
 		base._Ready();
 		GetNode<Button>("../../InitMenu/VBoxContainer/StatBtn").Pressed += InitGame;
 		Play(MainMenuAnimationName.WaitingToStart);
+		_song.Play();
+		AnimationStarted += OnStartAnimation;
 	}
+
+	private void OnStartAnimation(StringName name)
+	{
+		if (MainMenuAnimationName.InitGame.Equals(name))
+		{
+			_song.Stop();
+			_laugh.Play();
+		}
+	}
+
+
 
 	private void InitGame()
 	{
