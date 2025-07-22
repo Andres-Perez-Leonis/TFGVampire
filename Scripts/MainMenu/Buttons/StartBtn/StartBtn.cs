cb2bd915afb992473f1cdd2d3625582d86fd9bea
AndresPerez <apleonis@gmail.com>
2025-07-23 01:55:35 +0200

    Configured to work with a loading scene

--- Scripts/MainMenu/Buttons/StartBtn/StartBtn.cs
+++ Scripts/MainMenu/Buttons/StartBtn/StartBtn.cs
@@ -3,17 +3,25 @@ using System;
 
 public partial class StartBtn : Button
 {
+	private AnimationPlayer _animator;
 	[Export] private string _levelPath;
 
 	public override void _Ready()
 	{
+		_animator = Owner.GetParent().GetNode<AnimationPlayer>("animation/AnimationPlayer");
+		_animator.AnimationFinished += InitFirstLevel;
 		Pressed += InitFirstLevel;
 	}
 
-	private void InitFirstLevel()
+    private void InitFirstLevel(StringName animName)
+    {
+		if (MainMenuAnimationName.InitGame.Equals(animName))
+			GetTree().ChangeSceneToFile(_levelPath);
+    }
+
+    private void InitFirstLevel()
 	{
-		//if (Owner is ParentNodeMenu c) c.OnButtonClick();
-		GetTree().ChangeSceneToFile(_levelPath);
+		((Control)Owner).Visible = false;
 	}
 
 }
