6f27c60cbecc12086976ec1df17328d41f606bdf
AndresPerez <apleonis@gmail.com>
2025-07-23 11:59:53 +0200

    Animation Guard Names Added

---
+++ Resources/AnimationResources/AnimationNameGuard.cs
@@ -0,0 +1,18 @@
+using Godot;
+/**
+	Class for keep Animation names for NPC
+
+	- To avoid the magic string
+
+*/ 
+
+public partial class AnimationNameGuard : Resource
+{
+	public const string Idle = "Idle";
+	public const string Walking_Left = "Walking_Left";
+	public const string Walking_Right = "Walking_Right";
+	public const string Attack_Left = "Attack_Left";
+	public const string Attack_Right = "Attack_Right";
+	public const string Dragging_Left = "Dragging_Left";
+	public const string Dragging_Right = "Dragging_Right";
+}
